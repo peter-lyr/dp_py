244de531c0423197fe61af3ae73cde2a64ab9627
peter-lyr <llydrp.ldp@gmail.com>
2024-04-29 00:13:37 +0800

    <leader>prs M.run_sel()

---
+++ lua/.dp_py.lua.py/plot_one_line.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2024 liudepei. All Rights Reserved.
+# create at 2024/03/09 18:41:00 星期六
+
+
+import re
+import sys
+
+try:
+    import matplotlib.pyplot as plt
+except:
+    import os
+    os.system('pip install matplotlib -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host mirrors.aliyun.com')
+    import matplotlib.pyplot as plt
+
+
+def get_number_pattern():
+    decint = r"(?:[1-9](?:_?\d)*|0+(_?0)*)"
+    binint = r"(?:0[bB](?:_?[01])+)"
+    octint = r"(?:0[oO](?:_?[0-7])+)"
+    hexint = r"(?:0[xX](?:_?[0-9a-fA-F])+)"
+    integer = rf"(?:{decint}|{binint}|{octint}|{hexint})"
+
+    digitpart = r"(?:\d(?:_?\d)*)"
+    exponent = rf"(?:[eE][-+]?{digitpart})"
+    fraction = rf"(?:\.{digitpart})"
+    pointfloat = rf"(?:{digitpart}?{fraction}|{digitpart}\.)"
+    exponentfloat = rf"(?:(?:{digitpart}|{pointfloat}){exponent})"
+    floatnumber = rf"(?:{pointfloat}|{exponentfloat})"
+
+    number = re.compile(rf"^[-+]?(?:{integer}|{floatnumber})$")
+
+    return number
+
+
+patt = get_number_pattern()
+
+
+def get_nums_list_from_file(file):
+    with open(file, "rb") as f:
+        content = f.read().decode("utf-8")
+    numbers = []
+    temp = content.split()
+    for t in temp:
+        m = patt.match(t)
+        if not m:
+            continue
+        try:
+            num = eval(m[0])
+            numbers.append(num)
+        except Exception as e:
+            print(e)
+    return numbers
+
+
+def main():
+    if len(sys.argv) > 1:
+        y = get_nums_list_from_file(sys.argv[1])
+        if not y:
+            return
+    else:
+        return
+    plt.plot(y)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- lua/dp_py.lua
+++ lua/dp_py.lua
@@ -4,41 +4,72 @@ local sta, B = pcall(require, 'dp_base')
 
 if not sta then return print('Dp_base is required!', debug.getinfo(1)['source']) end
 
+M.source = B.getsource(debug.getinfo(1)['source'])
+M.lua = B.getlua(M.source)
+
 if B.check_plugins {
       'folke/which-key.nvim',
     } then
   return
 end
 
-function M.run_in_cmdline()
-  B.cmd('!chcp 65001 && %s', B.rep(B.buf_get_name()))
-end
+M.py_files = require 'plenary.scandir'.scan_dir(B.get_source_dot_dir(M.source, 'py'), { hidden = true, depth = 64, add_dirs = false, })
+print("vim.inspect(M.py_files):", vim.inspect(M.py_files))
 
-function M.run_in_terminal()
-  local file = B.rep(B.buf_get_name())
-  vim.cmd 'wincmd s'
-  local _sta, _ = pcall(vim.cmd, 'te')
-  if not _sta then
-    vim.cmd 'close'
-    return
+function M.run_in()
+  function M.run_in_cmdline()
+    B.cmd('!chcp 65001 && %s', B.rep(B.buf_get_name()))
   end
-  vim.api.nvim_chan_send(vim.b.terminal_job_id, file .. '\r')
-  vim.api.nvim_chan_send(vim.b.terminal_job_id, 'exit\r')
-  vim.cmd [[call feedkeys("i")]]
+
+  function M.run_in_terminal()
+    local file = B.rep(B.buf_get_name())
+    vim.cmd 'wincmd s'
+    local _sta, _ = pcall(vim.cmd, 'te')
+    if not _sta then
+      vim.cmd 'close'
+      return
+    end
+    vim.api.nvim_chan_send(vim.b.terminal_job_id, file .. '\r')
+    vim.api.nvim_chan_send(vim.b.terminal_job_id, 'exit\r')
+    vim.cmd [[call feedkeys("i")]]
+  end
+
+  function M.run_in_asyncrun()
+    local file = B.rep(B.buf_get_name())
+    B.cmd('AsyncRun %s', file)
+    vim.cmd 'copen'
+    vim.cmd 'wincmd J'
+    B.set_timeout(100, function()
+      local lines = vim.fn.line '$' + 1
+      local max = vim.fn.float2nr(vim.o.lines / 2)
+      vim.api.nvim_win_set_height(0, lines > max and max or lines)
+      vim.cmd 'norm zb'
+    end)
+  end
 end
 
-function M.run_in_asyncrun()
-  local file = B.rep(B.buf_get_name())
-  B.cmd('AsyncRun %s', file)
-  vim.cmd 'copen'
-  vim.cmd 'wincmd J'
-  B.set_timeout(100, function()
-    local lines = vim.fn.line '$' + 1
-    local max = vim.fn.float2nr(vim.o.lines / 2)
-    vim.api.nvim_win_set_height(0, lines > max and max or lines)
-    vim.cmd 'norm zb'
-  end)
+function M.run_sel_py()
+  function M._cmdline_enter(py_file)
+    local head = vim.fn.fnamemodify(py_file, ':h')
+    local tail = vim.fn.fnamemodify(py_file, ':t')
+    local args = vim.fn.input(string.format('python %s ', tail))
+    B.system_run_histadd('asyncrun', '%s && python %s %s', B.system_cd(head), tail, args)
+  end
+
+  function M.run_sel()
+    if #M.py_files == 0 then
+      return
+    elseif #M.py_files == 1 then
+      M._cmdline_enter(M.py_files[1])
+    else
+      B.ui_sel(M.py_files, 'sel run py', function(py_file)
+        M._cmdline_enter(py_file)
+      end)
+    end
+  end
 end
+
+M.run_in()
 
 require 'which-key'.register {
   ['<leader>p'] = { name = 'dp_py', },
@@ -46,6 +77,12 @@ require 'which-key'.register {
   ['<leader>prc'] = { function() M.run_in_cmdline() end, 'dp_py.run: run_in_cmdline', silent = true, mode = { 'n', 'v', }, },
   ['<leader>prt'] = { function() M.run_in_terminal() end, 'dp_py.run: run_in_terminal', silent = true, mode = { 'n', 'v', }, },
   ['<leader>pra'] = { function() M.run_in_asyncrun() end, 'dp_py.run: run_in_asyncrun', silent = true, mode = { 'n', 'v', }, },
+}
+
+M.run_sel_py()
+
+require 'which-key'.register {
+  ['<leader>prs'] = { function() M.run_sel() end, 'dp_py.run: run_sel', silent = true, mode = { 'n', 'v', }, },
 }
 
 return M
